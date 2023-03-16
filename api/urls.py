1255a09bc2c5b5c0686ae7679030c954bc308908
Snoolf001 <abdurahmonovjaloliddin2002@gmail.com>
2023-03-16 11:16:04 +0500

    change complete url to comlete_task

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from .views import get_task, get_tasks, complete, completed_tasks, incompleted_tasks, delete
+from .views import get_task, get_tasks, complete_task, completed_tasks, incompleted_tasks, delete_task
 urlpatterns = [
     path('tasks/', get_tasks),
     path('tasks/<int:pk>', get_task),
-    path('tasks/<int:pk>/complete', complete),
-    path('tasks/<int:pk>/delete', delete),
+    path('tasks/<int:pk>/complete', complete_task),
+    path('tasks/<int:pk>/delete', delete_task),
     path('tasks/completed', completed_tasks),
     path('tasks/incompleted', incompleted_tasks),
 
